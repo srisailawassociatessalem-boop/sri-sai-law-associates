394942e32dc96de682563e51721969c11a7b0529
srisailawassociatessalem-boop <srisailawassociates.salem@gmail.com>
2026-07-21 13:13:40 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { createServer } from "http";
 import path from "path";
 import { fileURLToPath } from "url";
+import { Resend } from "resend";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
